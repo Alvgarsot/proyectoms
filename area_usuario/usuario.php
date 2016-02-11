2fdd97c57e54363c95cb48b85431d221ad737baf
Alvaro-local <alvgarsot92@gmail.com>
2016-02-12 00:04:54 +0100

    SCRIPT TERMINADO 11-12/2/2016

--- area_usuario/usuario.php
+++ area_usuario/usuario.php
@@ -1,6 +1,28 @@
 
 <?php
   session_start();
+
+
+// -----------------
+                  if (isset($_GET['cancion'])) {
+$connection3 = new mysqli("localhost", "msadmin", "admin", "msalvaro");
+    $connection3->set_charset("utf8");
+                      $r=0;
+                      $j=0;
+    $siguiente_cancion[0]="ASEREJE";
+        if ($result3 = $connection3->query("SELECT nombre_cancion FROM lista , usuario, forma, cancion WHERE lista.nombre_usuariofk=usuario.nombre_usuario AND lista.id_lista=forma.id_listafk AND cancion.id_cancion=forma.id_cancionfk2 AND num_cancion>(SELECT num_cancion FROM lista , usuario, forma, cancion WHERE lista.nombre_usuariofk=usuario.nombre_usuario AND lista.id_lista=forma.id_listafk AND cancion.id_cancion=forma.id_cancionfk2 AND nombre_cancion='".$_GET['cancion']."' AND nombre_usuario='".$_SESSION['usuario']."' AND id_lista='".$_GET['id']."') AND nombre_usuario='".$_SESSION['usuario']."' AND id_lista='".$_GET['id']."' ORDER BY num_cancion asc;")) {
+              if ($result3->num_rows===0) {
+                echo "ERROR";
+              } else {
+                   while($obj3 = $result3->fetch_object()) {
+                     $siguiente_cancion[$r]=$obj3->nombre_cancion;
+                       $r++;
+                        
+                 }
+                 
+                 }
+              }
+          }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +36,9 @@
     <link rel="stylesheet" type="text/css" href="../estilos/general.css">
       <link href='https://fonts.googleapis.com/css?family=Dosis:600' rel='stylesheet' type='text/css'>
       <!-- ---------SCRIPT PARA PLAYLIST AUTOPLAY--------- 
+
+
+
       <script type="text/javascript">
 
 function setup() {
@@ -30,13 +55,28 @@ function setup() {
 
 }
 ------------------- -->
+       <script type="text/javascript">
+
+function setup() {
+    var k=0;
+    var siguiente= <?php echo json_encode($siguiente_cancion); ?>;
+    audioPlayer = document.getElementById('audio');
+    document.getElementById('audio').addEventListener('ended', function(){
+        audioPlayer.src ="../../aver/"+ siguiente[k];
+        audioPlayer.load();
+        audioPlayer.play();
+        k=k+1;
+        }, false);
+
+}
 </script>
   </head>
   <body onLoad="setup();">
 
     <!--  ----------------------CABECERA---------------------- -->
       <div class="cabecera">
-        <div id="cabecera1"><img src="../img/headphones.png"><p>Banner aqui con logo --------------<?php echo "BIENVENIDO ".strtoupper($_SESSION["usuario"]); ?></p></div>
+        <div id="cabecera1"><img src="../img/headphones.png"><p>Banner aqui con logo --------------<?php echo "BIENVENIDO ".strtoupper($_SESSION["usuario"]);
+           ?></p></div>
           <div id="cabecera2"><p><a href="cierre.php">Cerrar sesión</a></p></div>
       </div>
       <!-- --------------LISTAS---------------- -->
@@ -45,6 +85,7 @@ function setup() {
           <div class="liscont">
              <ul class="listausu">
                <?php
+
         if (!isset($_SESSION["usuario"])) {
           header("location: ../login.php");
           }
@@ -100,15 +141,19 @@ $connection2 = new mysqli("localhost", "msadmin", "admin", "msalvaro");
           }
 }
 else {
- /* --------------------------------------------------- */
+
             echo "<p>Primero elige tu lista de reproducción</p>";
 }
+ /* --------------------------------------------------- */
               ?>
         </div>
       </div>
       <div class="pie"><?php    if (isset($_GET["cancion"])) {
+                 
 echo "<audio id='audio' src='../../aver/".$_GET['cancion']."' controls='controls'><p>Este navegador es compatible con nuestro reproductor de música, rogamos lo intente de nuevo en otro navegador</p></audio>";
+                 var_dump($siguiente_cancion);
               }
           ?></div>
+
   </body>
 </html>
